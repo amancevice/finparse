8e9caca6ad072ba32383628bc2a83e5cc04b4604
Alexander Mancevice <amancevice@cargometrics.com>
2016-11-28 10:47:39 -0500

    Parametrized tests

--- tests/test_finparse.py
+++ tests/test_finparse.py
@@ -1,49 +1,57 @@
 # coding=utf-8
 
 import finparse
+import pytest
 
 
-def test_sign():
-    assert finparse.sign_string('$5.00') == (1, '$5.00')
-    assert finparse.sign_string('($5.00)') == (-1, '$5.00')
-    assert finparse.sign_string('(($5.00))') == (1, '$5.00')
-    assert finparse.sign_string('($5.00))') == (-1, '$5.00)')
-    assert finparse.sign_string('(($5.00)') == (-1, '($5.00')
-    assert finparse.sign_string('$100,000') == (1, '$100,000')
-    assert finparse.sign_string('$100,000.') == (1, '$100,000.')
-    assert finparse.sign_string('$100,000.111') == (1, '$100,000.111')
-    assert finparse.sign_string('$100.000,') == (1, '$100.000,')
+@pytest.mark.parametrize('given, sign, expected', [
+    ('$5.00', 1, '$5.00'),
+    ('($5.00)', -1, '$5.00'),
+    ('(($5.00))', 1, '$5.00'),
+    ('($5.00))', -1, '$5.00)'),
+    ('(($5.00)', -1, '($5.00'),
+    ('$100,000', 1, '$100,000'),
+    ('$100,000.', 1, '$100,000.'),
+    ('$100,000.111', 1, '$100,000.111'),
+    ('$100.000,', 1, '$100.000,'),
+])
+def test_sign(given, sign, expected):
+    assert finparse.sign_string(given) == (sign, expected)
 
 
-def test_strip():
-    assert finparse.strip('$5.00') == '5.00'
-    assert finparse.strip('($5.00)') == '5.00'
-    assert finparse.strip('(($5.00))') == '5.00'
-    assert finparse.strip('($5.00))') == '5.00'
-    assert finparse.strip('(($5.00)') == '5.00'
-    assert finparse.strip('$100,000') == '100000'
-    assert finparse.strip('$100,000.') == '100000.'
-    assert finparse.strip('$100,000.111') == '100000.111'
-    assert finparse.strip('$100.000,', ',') == '100000,'
+@pytest.mark.parametrize('given, decimal, expected', [
+    ('$5.00', '.', '5.00'),
+    ('($5.00)', '.', '5.00'),
+    ('(($5.00))', '.', '5.00'),
+    ('($5.00))', '.', '5.00'),
+    ('(($5.00)', '.', '5.00'),
+    ('$100,000', '.', '100000'),
+    ('$100,000.', '.', '100000.'),
+    ('$100,000.111', '.', '100000.111'),
+    ('$100.000,', ',', '100000,')])
+def test_strip(given, decimal, expected):
+    assert finparse.strip(given, decimal) == expected
 
 
-def test_parse():
-    assert finparse.parse('$5.00') == 5.
-    assert finparse.parse('($5.00)') == -5.
-    assert finparse.parse('(($5.00))') == 5.
-    assert finparse.parse('($5.00))') == -5.
-    assert finparse.parse('(($5.00)') == -5.
-    assert finparse.parse('$100,000') == 100000.
-    assert finparse.parse('$100,000.') == 100000.
-    assert finparse.parse('$100,000.111') == 100000.111
-    assert finparse.parse('$100.000,', ',') == 100000.
-    assert finparse.parse('1,234,567.89') == 1234567.89
-    assert finparse.parse('1234567.89') == 1234567.89
-    assert finparse.parse('1234567,89', ',') == 1234567.89
-    assert finparse.parse('1,234,567·89', '·') == 1234567.89
-    assert finparse.parse('1.234.567,89', ',') == 1234567.89
-    assert finparse.parse('1˙234˙567,89', ',') == 1234567.89
-    assert finparse.parse('1\'234\'567.89') == 1234567.89
-    assert finparse.parse('1\'234\'567,89', ',') == 1234567.89
-    assert finparse.parse('1.234.567’89', '’') == 1234567.89
-    assert finparse.parse('1.234.567,89', ',') == 1234567.89
+@pytest.mark.parametrize('given, decimal, expected', [
+    ('$5.00', '.', 5.),
+    ('($5.00)', '.', -5.),
+    ('(($5.00))', '.', 5.),
+    ('($5.00))', '.', -5.),
+    ('(($5.00)', '.', -5.),
+    ('$100,000', '.', 100000.),
+    ('$100,000.', '.', 100000.),
+    ('$100,000.111', '.', 100000.111),
+    ('$100.000,', ',', 100000.),
+    ('1,234,567.89', '.', 1234567.89),
+    ('1234567.89', '.', 1234567.89),
+    ('1234567,89', ',', 1234567.89),
+    ('1,234,567·89', '·', 1234567.89),
+    ('1.234.567,89', ',', 1234567.89),
+    ('1˙234˙567,89', ',', 1234567.89),
+    ('1\'234\'567.89', '.', 1234567.89),
+    ('1\'234\'567,89', ',', 1234567.89),
+    ('1.234.567’89', '’', 1234567.89),
+    ('1.234.567,89', ',', 1234567.89)])
+def test_parse(given, decimal, expected):
+    assert finparse.parse(given, decimal) == expected
