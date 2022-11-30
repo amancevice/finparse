bf0195501b780fdb2399cdb5f298cff359c8b81d
Alexander Mancevice <alexander.mancevice@hey.com>
2022-11-29 19:36:24 -0500

    black formatting

--- finparse/__init__.py
+++ finparse/__init__.py
@@ -1,38 +1,39 @@
 """ Module to parse a number-string to a float or decimal. """
 import re
 
-import pkg_resources
-
-try:
-    __version__ = pkg_resources.get_distribution(__package__).version
-except pkg_resources.DistributionNotFound:  # pragma: no cover
-    __version__ = None                      # pragma: no cover
+__version__ = "0.1.3"
 
 
 def sign_string(string, sign=1):
-    """ Determine what sign the string should be based on if it is wrapped
-        within parenthesis. """
+    """
+    Determine what sign the string should be based on if it is wrapped
+    within parenthesis.
+    """
     try:
         # Try to unwrap numbers wrapped in ()s
-        for match in re.match(r'^\((.*?)\)$', string).groups():
+        for match in re.match(r"^\((.*?)\)$", string).groups():
             return sign_string(match, sign * -1)
     except AttributeError:
         # Check for a negative sign in the string before returning
-        return -int('-' in string) or sign, string
+        return -int("-" in string) or sign, string
 
 
-def strip(string, decimal='.'):
-    """ Strip non-decimal characters out of string. """
-    return re.sub(r'[^\d{}]+'.format(decimal), '', string)
+def strip(string, decimal="."):
+    """
+    Strip non-decimal characters out of string.
+    """
+    return re.sub(r"[^\d{}]+".format(decimal), "", string)
 
 
-def parse(string, decimal='.', cast=None):
-    """ Parse a string to a number object. """
+def parse(string, decimal=".", cast=None):
+    """
+    Parse a string to a number object.
+    """
     string = str(string)
     cast = cast or float
-    pct = string.endswith('%')
+    pct = string.endswith("%")
     sign, string = sign_string(string)
     string = strip(string, decimal)
-    string = string.replace(decimal, '.')
-    value = cast(string) * sign / (100. ** pct)
+    string = string.replace(decimal, ".")
+    value = cast(string) * sign / (100.0**pct)
     return value
